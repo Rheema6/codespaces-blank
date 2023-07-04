9f7c1035d31842edbc67999f779a58b00ba0db7c
Rheema6 <138562922+Rheema6@users.noreply.github.com>
2023-07-04 07:12:50 +0000

    first commit

---
+++ ASCII.cpp
@@ -0,0 +1,25 @@
+
+
+
+   #include <iostream>
+
+    using namespace std;
+    int intToAscii(int number) {
+     return '0' + number;  
+}
+
+int main() {
+
+ int number;
+
+ cout<<"The ASCII of " << 5 <<" is " <<intToAscii(5)<<endl;
+
+intToAscii(5);
+
+cout<<"The ASCII of "<< 8 <<" is " <<intToAscii(8)<<endl;
+
+intToAscii(8);
+
+}
+
+ 
